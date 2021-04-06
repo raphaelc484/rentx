d2044bc0eeaf3f455969c87ee5540e235a4a8d9c
raphaelc484 <raphaelc484@hotmail.com>
2021-04-06 20:46:20 -0300

    up

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
-import { CreateRentalController } from "@modules/rentals/useCases/CreateRentalController";
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticate } from "../middleware/ensureAuthenticate";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticate, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticate,
+  devolutionRentalController.handle
+);
 
 export { rentalRoutes };
